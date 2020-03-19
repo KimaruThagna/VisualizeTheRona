e484e14acf301bf26621e6a3ea9c705e875ff470
Kimaru thagana <macbuntu@192.168.1.5>
2020-03-19 15:47:58 +0300

    display results for total confirmed

--- folium_approach.py
+++ folium_approach.py
@@ -13,15 +13,15 @@ covid_data_rich = pd.merge(df, countries_with_coordinates, on='Country')
 # add a latitude and logitude property to the countries json objects for plotting
 
 # initialize map
-africa = folium.Map([-0.7832, 28.5085], zoom_start=3.42)
+africa = folium.Map([-0.7832, 28.5085], zoom_start=3.48)
 folium.TileLayer(tiles='Stamen Toner',name="Stamen Toner").add_to(africa)
 folium.TileLayer(tiles='Stamen Terrain',name="Stamen Terrain").add_to(africa)
 folium.TileLayer(tiles='CartoDB dark_matter',name="CartoDB Dark_Matter").add_to(africa)
 folium.LayerControl().add_to(africa)
 
-for country in covid_data_rich.iterrows():
+for idx, country in covid_data_rich.iterrows():
     # generate and display marker
-    icon = folium.DivIcon(html=f'<font color="red">{country.NewDeaths}</font>')
-    folium.Marker([country.latitude, country.longitude], icon=icon).add_to(africa)
+    icon = folium.DivIcon(html=f'<font color="red">{country["TotalConfirmed"]}</font>')
+    folium.Marker([country['latitude'], country['longitude']], icon=icon).add_to(africa)
 # save to html
 africa.save('covid.html')
