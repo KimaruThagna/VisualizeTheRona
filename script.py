833ba12b4f088059235b9c03945f5e70d1a32cb8
Kimaru thagana <macbuntu@192.168.1.5>
2020-03-19 13:19:16 +0300

    read covid datta as summary

--- script.py
+++ script.py
@@ -2,6 +2,9 @@ import geopandas as gpd
 import requests
 
 # download data from covid api
+url = 'https://api.covid19api.com/summary'
+covid_data = requests.get(url)
+print(covid_data.json())
 # Read in shapefile and examine data
 africa = gpd.read_file('shapefiles/Africa.shp')
 print(africa.head())
