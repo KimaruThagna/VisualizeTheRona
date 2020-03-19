2b796e2c63dff6ebb0425894108ff3d146ee3932
Kimaru thagana <macbuntu@192.168.1.5>
2020-03-19 14:24:28 +0300

    add csv files for filtering african countries

--- folium_approach.py
+++ folium_approach.py
@@ -1,8 +1,19 @@
-import folium
+import folium, requests
 
-africa = folium.Map([-8.7832, 34.5085], zoom_start=8)
+# download data from covid api
+url = 'https://api.covid19api.com/summary'
+covid_data = requests.get(url)
+covid_data = covid_data.json()
+print(covid_data['Countries'])
+
+# initialize map
+africa = folium.Map([-0.7832, 28.5085], zoom_start=3.42)
 folium.TileLayer(tiles='Stamen Toner',name="Stamen Toner").add_to(africa)
 folium.TileLayer(tiles='Stamen Terrain',name="Stamen Terrain").add_to(africa)
 folium.TileLayer(tiles='CartoDB dark_matter',name="CartoDB Dark_Matter").add_to(africa)
 folium.LayerControl().add_to(africa)
+
+icon = folium.DivIcon(html="<font color='red'>20</font>")
+folium.Marker([-0.7832, 28.5085], icon=icon).add_to(africa)
+# save to html
 africa.save('covid.html')
--- script.py
+++ script.py
@@ -1,13 +1,10 @@
 import geopandas as gpd
 import requests
-from matplotlib import pyplot as plt
+
 # download data from covid api
 url = 'https://api.covid19api.com/summary'
 covid_data = requests.get(url)
-
+print(covid_data.json())
 # Read in shapefile and examine data
 africa = gpd.read_file('shapefiles/Africa.shp')
-africa.plot()
-plt.axis("off")
-plt.show()
-
+print(africa.head())
